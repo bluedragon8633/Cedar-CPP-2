6a81afdd8d1f44f677cbf228f811c463d985884c
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-10-12 13:42:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
+#include<iostream>
+#include<vector>
+
 
 class frameObj {
+public:
 	int sourceX,sourceY;
 	bool flip;
-	int width,height;
+	int imgWidth,imgHeight;
+};
+
+class animationObj { //just an animation
+public:
+	int animId;
+	vector<frameObj> frames;
+	
+	
 };
 
 class baseObj {
@@ -13,16 +25,22 @@ public:
 	
 };
 
-class animObj : public baseObj {
+class billboardObj : frameObj { //static image. Can't be interacted with. Doesn't move. Ex. untouchable walls, plain ground tiles
 public:
 	
 };
 
-class interactiveObj : public animObj {
+class animObj : public baseObj { //non-interactive animated surface, ex. particles, smoke, animated ground tiles
+public:
+	int frameId,frameCounter,animationId;
+	vector<animationObj> anims;
+};
+
+class interactiveObj : public animObj { //can be interacted with. ex. walls, liquids, pits, spikes, pushable terrain
 public:
 	int xv, yv;
 	bool isHarmful;
-	
+	bool isSolid;
 	
 	
 	
@@ -33,3 +51,6 @@ public:
 	}
 };
 
+class player : public interactiveObj {
+	
+};
