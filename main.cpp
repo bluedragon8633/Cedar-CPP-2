8173c6f692dc56434f6d61dfcd38d6b14626a97f
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-10-13 19:00:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,51 @@
 #include<iostream>
+#include<string>
+#include <fstream>
 #include<vector>
+using namespace std;
+
+
+int findInt(string name) {
+    string plainText;
+    ifstream ScrnData("./assets/presets.txt");
+    for (int i = 0; i < 3; i++) {
+        if (plainText.find(name) != string::npos) {
+            break;
+        }
+        getline(ScrnData, plainText);
+    }
+    int startOfValue = 0;
+    for (int i = 0; i < plainText.length(); i++) {
+        if (plainText[i] == ':') {
+            startOfValue = i + 1;
+            break;
+        }
+    }
+    return stoi(plainText.substr(startOfValue, plainText.length()));
+}
+
+
+
 
 class game { //contains general info. TileSize, WindowSize
 public:
-	int 
+    int tileWidth,tileHeight;
+    int tileSize;
 };
 
 class frameObj {
 public:
-	int sourceX,sourceY;
+	int sourceX, sourceY;
 	bool flip;
-	int imgWidth,imgHeight;
+	int imgWidth, imgHeight;
 };
 
 class animationObj { //single animation. <animObj> contains multiple of these to make a fully realized character.
 public:
 	int animId;
 	vector<frameObj> frames;
-	
-	
+
+
 };
 
 class baseObj {
@@ -26,17 +53,17 @@ public:
 	int x, y;
 	int dir;
 	int width, height;
-	
+
 };
 
 class billboardObj : frameObj { //static image. Can't be interacted with. Doesn't move. Ex. untouchable walls, plain ground tiles
 public:
-	
+
 };
 
 class animObj : public baseObj { //non-interactive animated surface, ex. particles, smoke, animated ground tiles
 public:
-	int frameId,frameCounter,animationId;
+	int frameId, frameCounter, animationId;
 	vector<animationObj> anims;
 };
 
@@ -45,16 +72,23 @@ public:
 	int xv, yv;
 	bool isHarmful;
 	bool isSolid;
-	
-	
-	
-	
-	void move(xin,yin) {
+
+
+
+
+	void move(int xin, int yin) {
 		x = xin;
 		y = yin;
 	}
 };
 
 class player : public interactiveObj {
-	
+
 };
+
+int main() {
+    cout << "playerStartX: " << findInt("playerStartX") << endl;
+    cout << "playerStartY: " << findInt("playerStartY") << endl;
+    cout << "playerStartLvl: " << findInt("playerStartLvl") << endl;
+    return 0;
+}
