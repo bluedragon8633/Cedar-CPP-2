271618772c32e5d0bdc87ac2f8fcd41828faacb4
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-10-26 09:49:47 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,14 @@
 #include"SFML/Graphics.hpp"
 using namespace std;
 
-
+sf::Texture atlas;
+sf::Font font;
+sf::RectangleShape rect;
+sf::Text textObj;
+sf::Sprite block;
+sf::Sprite tile;
+sf::Event e;
+sf::RenderWindow window;
 
 
 
@@ -20,39 +27,20 @@ using namespace std;
 int main() {
     
 	GameConsts game("Mimi's Grand Journey!");
-	Drawer d(game);
 	PlayerObj player;
-    cout << "frameLimit: " << game.FRAME_LIMIT << endl;
-    cout << "scrnSize: " << game.scrnWidth << "," << game.scrnHeight << endl;
-    /*
-    
-	while (d.window.isOpen()) {
-        while (d.window.pollEvent(d.e))
-        {
-            if (d.e.type == sf::Event::Closed)
-                d.window.close();
-        }
 
-
-        d.window.display();
-	}
-    return 0;*/
-    d.window.setFramerateLimit(game.FRAME_LIMIT);
-    while (d.window.isOpen())
+    window.setFramerateLimit(game.FRAME_LIMIT);
+    while (window.isOpen())
     {
-        // Event processing
         sf::Event event;
-        while (d.window.pollEvent(event))
+        while (window.pollEvent(event))
         {
-            // Request for closing the window
             if (event.type == sf::Event::Closed)
-                d.window.close();
+                window.close();
         }
-        // Clear the whole window before rendering a new frame
-        d.window.clear();
-        // Draw some graphical entities
-        // End the current frame and display its contents on screen
-        d.window.display();
+
+        window.clear();
+        window.display();
     }
     return 0;
 }
