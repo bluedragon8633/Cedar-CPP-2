cd3ee972fea9c674ada6e0b33a949dbdacb2f332
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-10-26 10:00:06 -0400

    Update Drawer.h

--- Drawer.h
+++ Drawer.h
@@ -4,20 +4,19 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
 namespace Drawing {
-     extern sf::Texture atlas;
-     extern sf::Font font;
-     extern sf::RectangleShape rect;
-     extern sf::Text textObj;
-     extern sf::Sprite block;
-     extern sf::Sprite tile;
-     extern sf::Sprite blockBullet;
-     extern sf::RenderWindow window;
-     extern sf::RectangleShape rectangle;
-     extern sf::Event e;
-
+    extern sf::Texture atlas;
+    extern sf::Font font;
+    extern sf::RectangleShape rect;
+    extern sf::Text textObj;
+    extern sf::Sprite block;
+    extern sf::Sprite tile;
+    extern sf::RenderWindow window;
+    extern sf::RectangleShape rectangle;
+    extern sf::Event e;
+	const int shadowAlpha = 200;
 
     void resizeWindow(std::string title, int width, int height);
-
+	void setup();
     void loadTextures();
     void stamp(int x, int y, int width, int height, int drawx, int drawy, bool centerOrigin);
     void stamp(animObj a);
