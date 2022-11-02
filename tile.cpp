06b6e686fd288de88143c588d97bd18a8a1e5de0
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-11-02 09:18:47 -0400

    Create tile.cpp

---
+++ tile.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"Drawer.h"
+#include"game.h"
+#include"InteractiveObjs.h"
+#include"tile.h"
+
+TileMap() {
+	
+}
+
+TileMap(int level) {
+	
+}
+	
+void resize(int nWidth,int nHeight) {
+	
+}
+
+void draw(int xin,int yin, int nWidth, int nHeight) {
+	
+}
