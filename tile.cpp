267a8f4f1ab0e58c464837f4c445572996611e0c
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-11-02 10:30:52 -0400

    Update tile.cpp

--- tile.cpp
+++ tile.cpp
@@ -4,6 +4,66 @@
 #include"InteractiveObjs.h"
 #include"tile.h"
 
+int TileMap::getTileCoord(int xin, int yin, char retType) {
+    char returnType = retType;
+    int tilePointer = 0;
+    string tileID;
+
+
+    if (yin == 0) {
+        tileID = tileID + "1";
+    }
+    else if (level::contains(level::wallObjs, level::numOfWallObjs, level::tiles[xin][yin - 1])) {
+        tileID = tileID + "1";
+    }
+    else {
+        tileID = tileID + "0";
+    }
+    if (xin == 0) {
+        tileID = tileID + "1";
+    }
+    else if (level::contains(level::wallObjs, level::numOfWallObjs, level::tiles[xin - 1][yin])) {
+        tileID = tileID + "1";
+    }
+    else {
+        tileID = tileID + "0";
+    }
+    if (yin == 9) {
+        tileID = tileID + "1";
+    }
+    else if (level::contains(level::wallObjs, level::numOfWallObjs, level::tiles[xin][yin + 1])) {
+        tileID = tileID + "1";
+    }
+    else {
+        tileID = tileID + "0";
+    }
+
+    if (xin == 9) {
+        tileID = tileID + "1";
+    }
+    else if (level::contains(level::wallObjs, level::numOfWallObjs, level::tiles[xin + 1][yin])) {
+        tileID = tileID + "1";
+    }
+    else {
+        tileID = tileID + "0";
+    }
+
+
+
+
+    //cout << tileID << "; ";
+    if (xin == level::scrnTileWidth - 1) {
+        //cout << "\n";
+    }
+    if (returnType == 'X') {
+        return tilePosCoords[index(tileCode, 15, tileID)][0];
+    }
+    else {
+        return tilePosCoords[index(tileCode, 15, tileID)][1] + level::PAL * 5;
+    }
+    return 0;
+}
+
 TileMap::TileMap() {
 	
 }
